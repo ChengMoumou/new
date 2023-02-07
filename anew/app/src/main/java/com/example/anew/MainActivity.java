d29361b646f2ac85a0a67eea0cb6d37c3b224f28
chengmeiling <chengmeiling@hsht.com>
2023-02-07 20:24:59 +0800

    test1

--- anew/app/src/main/java/com/example/anew/MainActivity.java
+++ anew/app/src/main/java/com/example/anew/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.anew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
-
+public final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("好久不见");
+        Log.d(TAG, "onCreate: 好久不见");
     }
 }
